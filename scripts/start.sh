e7ba4577aa7dd62a47c6d0b34e01fb36724ffe11
Simeon Zahariev <simeon.zahariev@chaosgroup.com>
2024-05-12 22:43:09 +0300

    trim

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f docker-compose.yml ]; then
-	echo 'Almost there! You need to run ./init.sh first :)'
-	exit 1;
-fi
-
-exec docker-compose up -d
--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-docker-compose pull
-docker-compose up -d
